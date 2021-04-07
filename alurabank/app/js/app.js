4c5301ed0307cf981e95a8f73837a020eb761689
Jabis dos Santos de Andrade <jabis.andrade@serpro.gov.br>
2021-04-07 16:07:18 -0300

    Utilização da biblioteca Jquery

--- alurabank/app/js/app.js
+++ alurabank/app/js/app.js
@@ -1,5 +1,3 @@
 // app/ts/app.ts
 const controller = new NegociacaoController();
-document
-    .querySelector('.form')
-    .addEventListener('submit', controller.adiciona.bind(controller));
+$('.form').submit(controller.adiciona.bind(controller));
