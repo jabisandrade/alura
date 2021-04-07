323dbd987c6c1a20540b66ef04c500e7c86aa98c
Jabis dos Santos de Andrade <jabis.andrade@serpro.gov.br>
2021-04-07 15:14:21 -0300

    Transaformando em classe abstrata

--- alurabank/app/ts/views/View.ts
+++ alurabank/app/ts/views/View.ts
@@ -1,4 +1,4 @@
-class View<T> {
+abstract class View<T> {
 
     private _elemento: Element;
 
@@ -12,10 +12,7 @@ class View<T> {
         this._elemento.innerHTML = this.template(model);
     }
 
-    template(model: T): string {
+    abstract template(model: T): string ;
 
-        throw new Error('Você deve implementar o método template');
-        
-    }
 
 }
