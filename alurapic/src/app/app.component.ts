25164d0416f6f6c642d948c58103c6b3ed9fdf84
Jabis Andrade <jabis.andrade@gmail.com>
2021-05-06 16:52:12 -0300

    Isolando acesso em serviços

--- alurapic/src/app/app.component.ts
+++ alurapic/src/app/app.component.ts
@@ -1,16 +1,19 @@
-import { HttpClient } from '@angular/common/http';
-import { Component } from '@angular/core';
+import { HttpClient } from "@angular/common/http";
+import { Component } from "@angular/core";
+import { PhotoService } from "./photo/photo.service";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"],
 })
 export class AppComponent {
   photos = [];
-  constructor(http: HttpClient) {
-    http
-    .get<Object[]>('http://localhost:3000/flavio/photos')
-    .subscribe(photos => this.photos = photos);
-}
+  constructor(photoService: PhotoService) {
+    photoService
+      .listFromUser('flavio')
+      .subscribe(photos => this.photos = photos);
+
+  }
+
 }
